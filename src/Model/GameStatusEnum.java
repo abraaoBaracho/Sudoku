39dbff2a87b2d27152d374caf6721e31c00c6277
abraaoBaracho <abraao-50@hotmail.com>
2025-02-26 14:34:29 -0300

    Criando o jogo SUDOKU

---
+++ src/Model/GameStatusEnum.java
@@ -0,0 +1,19 @@
+package Model;
+
+public enum GameStatusEnum {
+
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+}
